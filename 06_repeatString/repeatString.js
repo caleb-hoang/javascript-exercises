d9215db75033bdd9e5b9b2e7055a3cb662df85e3
calebhoang <calebhoangm@gmail.com>
2025-11-25 16:10:30 -0600

    Completed 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnVal = ""
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++) {
+        returnVal = returnVal.concat(string)
+    }
+    return returnVal
 };
 
 // Do not edit below this line
