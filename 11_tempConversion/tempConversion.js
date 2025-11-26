33c7cd50ce9ffc48a868b967b193be149103ded4
calebhoang <calebhoangm@gmail.com>
2025-11-26 17:29:44 -0600

    Completed 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let untruncated = (temp - 32) * 5 / 9
+    let truncated = Math.round(Math.trunc(untruncated * 100) / 10) / 10
+    return truncated
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let untruncated = (temp * 9 / 5) + 32
+    let truncated = Math.round(Math.trunc(untruncated * 100) / 10) / 10
+    return truncated
 };
 
 // Do not edit below this line
