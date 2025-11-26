8897f774046b2f7339d66ae8a0f584e6a364dc23
calebhoang <calebhoangm@gmail.com>
2025-11-26 17:15:11 -0600

    Completed 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    if (!Number.isInteger(val1) || !Number.isInteger(val2)) {
+        return "ERROR"
+    }
 
+    if (val1 < 0 || val2 < 0) {
+        return "ERROR"
+    }
+
+    let biggest = Math.max(val1, val2)
+    let smallest = Math.min(val1, val2)
+
+    let sum = 0
+    for(let i = smallest; i <= biggest; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
