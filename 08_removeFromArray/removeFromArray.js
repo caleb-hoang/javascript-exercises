6a111726286d26f8e9cd0b01914a8ab89c86ff9e
calebhoang <calebhoangm@gmail.com>
2025-11-26 17:09:45 -0600

    Completed 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(items) {
+    let args = Array.prototype.slice.call(arguments, 1)
+    
+    for(let i = 0; i < items.length; i++) {
+        if (args.includes(items[i])) {
+            items.splice(i, 1)
+            i--
+        }
+    }
+    return items    
 };
 
 // Do not edit below this line
