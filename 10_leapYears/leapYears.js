bd0d99642bcd5540d00dc4c3d3e60e6d15100a5c
calebhoang <calebhoangm@gmail.com>
2025-11-26 17:20:36 -0600

    Completed 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let div4 = (year % 4) == 0
+    let div10 = (year % 10) == 0
+    let div400 = (year % 400) == 0
 
+    if (div4) {
+        if (div10) {
+            return div400
+        } else {
+            return true
+        }
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
