9021683b79d631b4b145c373857ff6809fc143d5
calebhoang <calebhoangm@gmail.com>
2025-12-06 15:08:48 -0600

    Completed 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let startIndex = 0
+    let endIndex = input.length - 1
 
+    while (startIndex <= endIndex) {
+        // make sure that checked chars are valid
+        while (!/^[a-zA-Z]$/.test(input.charAt(startIndex) && Number.isInteger(input.charAt(startIndex)))) {
+            startIndex ++
+        }
+        while (!/^[a-zA-Z]$/.test(input.charAt(endIndex) && Number.isInteger(input.charAt(endIndex)))) {
+            endIndex --
+        }
+
+        if (input.charAt(startIndex) != input.charAt(endIndex)) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
