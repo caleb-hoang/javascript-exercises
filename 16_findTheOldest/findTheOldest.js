148a82469fd9404271982930fea6d6415df649d9
calebhoang <calebhoangm@gmail.com>
2025-12-06 16:07:51 -0600

    Completed 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        older = calculateAge(oldest) > calculateAge(person)
+        if (older) {
+            return oldest
+        } else {
+            return person
+        }
+    }, people[0])
 };
+
+function calculateAge(person) {
+    let birth = person.yearOfBirth
+    let death = person.yearOfDeath
+
+    if (isNaN(death)) {
+        death = 2025
+    }
+
+    return death - birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
